e6ac328aaf893c00521c305b7237095203501d2f
bladecs <bladecs86@gmail.com>
2024-04-07 19:28:51 +0700

    add landing page

--- resources/views/landing/layout_landing_page.blade.php
+++ resources/views/landing/layout_landing_page.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+    <title>@yield('landing')</title>
+    <link rel="stylesheet" href="{{asset('css/landing.css')}}">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
     <link rel="stylesheet" href="{{asset('css/login_register.css')}}">
 </head>
 <body>
     @yield('content')
-    <script src="{{asset('js/swap.js')}}"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
