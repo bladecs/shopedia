c159283781f64bb8adf1ea5b70611c8593617216
bladecs <bladecs86@gmail.com>
2024-04-08 10:19:30 +0700

    add footer

--- resources/views/landing/landing_page.blade.php
+++ resources/views/landing/landing_page.blade.php
@@ -167,6 +167,57 @@
     </div>
 </div>
 <footer>
-
+    <div class="list-footer">
+        <div class="list-text">
+            <ul>
+                <li><h1>SHOPEDIA</h1></li>
+                <li><h2>Tentang Kami</h2></li>
+                <li><h2>Blog</h2></li>
+                <li><h2>Mitra Blog</h2></li>
+                <li><h2>Hak Kekayaan</h2></li>
+            </ul>
+            <ul>
+                <li><h1>BELI</h1></li>
+                <li><h2>Tagihan & Top Up</h2></li>
+                <li><h2>Tukar Tambah Handphone</h2></li>
+                <li><h2>Shopedia COD</h2></li>
+            </ul>
+        </div>
+        <div class="list-text">
+            <ul>
+                <li><h1>BELI</h1></li>
+                <li><h2>Tagihan & Top Up</h2></li>
+                <li><h2>Tukar Tambah Handphone</h2></li>
+                <li><h2>Shopedia COD</h2></li>
+            </ul>
+            <ul>
+                <li><h1>BELI</h1></li>
+                <li><h2>Tagihan & Top Up</h2></li>
+                <li><h2>Tukar Tambah Handphone</h2></li>
+                <li><h2>Shopedia COD</h2></li>
+            </ul>
+            <ul>
+                <li><h1>BELI</h1></li>
+                <li>
+                    <div class="sosmed">
+                        <ion-icon name="logo-facebook"></ion-icon>
+                        <ion-icon name="logo-twitter"></ion-icon>
+                        <ion-icon name="logo-instagram"></ion-icon>
+                        <ion-icon name="logo-linkedin"></ion-icon>
+                        <ion-icon name="logo-pinterest"></ion-icon>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="adds">
+        <img src="{{asset('images/logo_footer_sample.png')}}" alt="">
+        <div class="apps-store">
+            <button type="button">Google Play</button>
+            <button type="button">App Store</button>
+            <button type="button">AppGallery</button>
+        </div>
+        <h2>&copy 2022 - 2024 PT. Shopedia.</h2>
+    </div>
 </footer>
 @endsection
